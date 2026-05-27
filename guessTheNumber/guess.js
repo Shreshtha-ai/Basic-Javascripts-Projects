4dd99867e7f6072903678b5a0888fd9ec0b552b2
Shreshtha-ai <shreshthaagarwal2007@gmail.com>
2026-05-27 21:14:34 +0530

    feat: implement core game logic for guess the number project

--- guessTheNumber/guess.js
+++ guessTheNumber/guess.js
@@ -13,5 +13,73 @@ let prevGuess = []
 let numGuess = 1;
 let playgame = true;
 
+if(playgame){
+    submit.addEventListener('click',function(e){
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        console.log(guess);
+        validateGuess(guess)
+        
+    })
+}
+
+function validateGuess(guess){
+    if(isNaN(guess)){
+        alert('Please enter a valid number')
+    }else if(guess < 1){
+        alert('Please enter a number greater than 1')
+    }else if(guess > 100){
+        alert('Please enter a number less than 100')
+    }else{
+        prevGuess.push(guess)
+        if(numGuess === 11){
+            displayGuess(guess)
+            displayMessage(`Game over. The random number was ${randomNumber}`)
+            endGame()
+        }
+        else{
+            displayGuess(guess)
+            checkGuess(guess)
+        }
+
+    }
+
+}
+
+function checkGuess(guess){
+if(guess === randomNumber){
+    displayMessage(`You guessed the number`)
+    endGame()
+}
+else if(guess < randomNumber){
+    displayMessage(`Number is too low`)
+}
+else if(guess > randomNumber){
+    displayMessage(`Number is too high`)
+}
+
+}
+
+function displayGuess(guess){
+    userInput.value = ''
+    guessSlot.innerHTML += `${guess} `
+    numGuess++
+    remaining.innerHTML = `${11 - numGuess}`
+
+}
+
+function displayMessage(Message){
+lowOrHi.innerHTML = `<h2>${Message}</h2>`
+}
+
+function endGame(){
+userInput.value = ''
+userInput.setAttribute('disabled', '')
+
+}
+
+function newGame(){
+
+}
 
 
